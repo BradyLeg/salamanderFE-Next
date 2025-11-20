32605fef5be753295c6b4e6868645288fbc81bfb
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-11-20 13:44:02 -0800

    working on threshold display

--- app/components/ProcessorStartCard.jsx
+++ app/components/ProcessorStartCard.jsx
@@ -1,8 +1,16 @@
 import Image from 'next/image'
 import salamanderImg from '@/public/salamander.jpg'
 import DropDown from '@/app/components/imgCondition/DropDown';
+import { useState, useEffect } from "react";
 
 export default function ProcessorStartCard() {
+    const [rangeNum, setNum] = useState(60);
+
+    function rangeDisplay(event) {
+
+    }
+
+
     return (
         <div className="container-card-starter">
             <div className="card-row">
@@ -10,7 +18,8 @@ export default function ProcessorStartCard() {
                     <ul>
                         <li>Import Video <DropDown /></li>
                         <li>Color <input type="color" id="color" name="color"></input></li>
-                        <li>Threshold <input type="range" min="1" max="100"></input></li>
+                        <li>Threshold <input type="range" min="1" max="100" defaultValue={rangeNum} onInput={rangeDisplay}>
+                            <p></p></input></li>
                     </ul>
                 </div>
                 <div className="card-right">
